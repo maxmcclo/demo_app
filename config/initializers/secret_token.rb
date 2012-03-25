cade9797b6d1e87d2207c04e71aa641c642207f1
Michael McCloskey <maxmcclo@tig.com.au>
2012-03-25 18:42:32 +1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '52dee22dbe49fe240c56169ce47a2eb87aaccc6ed25c53dc0d669452e4cafb04c9bbb74141c89c464d90f4aab6c5d90f31d7b47c8ac7d328c7fc587535e1cc5e'
